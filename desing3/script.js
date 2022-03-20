60e3b16d4f35a124a5508f693961e67111e1af27
yanalalmer@yahoo.com <yanalalmir@Yanals-Mac-Pro.local>
2022-03-20 13:22:30 +0100

    design 3 added script

---
+++ desing3/script.js
@@ -0,0 +1,56 @@
+let slideIndex = 1;
+const inputs = document.querySelectorAll('input');
+const icons = document.querySelectorAll('.icon');
+showSlides(slideIndex);
+
+// icon select
+const selectIcon = (e) => {
+  e.currentTarget.classList.toggle('active');
+};
+
+// move to next slide
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+icons.forEach((icon) => {
+  icon.addEventListener('click', selectIcon);
+});
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName('card-slide');
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slides[slideIndex - 1].style.display = 'block';
+}
+// drag card
+const card = document.querySelector('.card-container');
+const header = card.querySelector('header');
+
+function onDrag({ movementX, movementY }) {
+  let getStyle = window.getComputedStyle(card);
+  let left = parseInt(getStyle.left);
+  let top = parseInt(getStyle.top);
+  card.style.left = `${left + movementX}px`;
+  card.style.top = `${top + movementY}px`;
+}
+
+header.addEventListener('mousedown', () => {
+  header.addEventListener('mousemove', onDrag);
+});
+document.addEventListener('mouseup', () => {
+  header.removeEventListener('mousemove', onDrag);
+});
